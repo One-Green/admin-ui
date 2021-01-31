00c9102e922a3f3c03a53b068db358e62ad7678a
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-01-31 02:31:49 +0100

    feat(settings.py): include water configuration endpoint

--- settings.py
+++ settings.py
@@ -5,5 +5,7 @@ API_BASIC_AUTH_USERNAME: str = os.getenv("API_BASIC_AUTH_USERNAME")
 API_BASIC_AUTH_PASSWORD: str = os.getenv("API_BASIC_AUTH_PASSWORD")
 BASIC_AUTH: tuple = (API_BASIC_AUTH_USERNAME, API_BASIC_AUTH_PASSWORD)
 
+WATER_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "water/config")
+
 SPRINKLER_REGISTRY: str = os.path.join(ONE_GREEN_CORE_API, "sprinkler/registry")
 SPRINKLER_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "sprinkler/config")
