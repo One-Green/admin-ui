bcd870033812de9a99d46cd2fbc333227ea9a89d
Vigneswaran Shanmugathas <shanmugathas.vigneswaran-prestataire@laposte.fr>
2021-01-31 02:32:07 +0100

    feat(core.api.water): add water config api interaction

---
+++ core/api/water.py
@@ -0,0 +1,42 @@
+import requests
+
+
+def get_configuration(
+        api: str,
+        _basic_auth: tuple = None
+) -> dict:
+    """
+    Configure water tank
+    :param api:
+    :param _basic_auth:
+    :return:
+    """
+    if _basic_auth:
+        return requests.get(api, basic_auth=_basic_auth).json()
+    else:
+        return requests.get(api).json()
+
+
+def post_configuration(
+        api: str,
+        config: dict,
+        _basic_auth: tuple = None
+) -> bool:
+    """
+    Configure a sprinkler with is tag
+    :param api:
+    :param config:
+    :param _basic_auth:
+    :return:
+    """
+
+    if _basic_auth:
+        if requests.post(api, json=config, basic_auth=_basic_auth).json()["acknowledge"]:
+            return True
+        else:
+            return False
+    else:
+        if requests.post(api, json=config).json()["acknowledge"]:
+            return True
+        else:
+            return False
