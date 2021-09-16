51e9ea1046dd84e18472cac69f7ceba0058a1340
shanisma <shanmugathas.vigneswaran@outlook.fr>
2021-09-16 19:16:07 +0200

    feat(settings.py): update ONE_GREEN_API_URL env var

--- settings.py
+++ settings.py
@@ -1,18 +1,20 @@
 import os
 
-ONE_GREEN_CORE_API: str = os.getenv("ONE_GREEN_CORE_API")
+ONE_GREEN_API_URL: str = os.getenv("ONE_GREEN_API_URL")
 API_BASIC_AUTH_USERNAME: str = os.getenv("API_BASIC_AUTH_USERNAME")
 API_BASIC_AUTH_PASSWORD: str = os.getenv("API_BASIC_AUTH_PASSWORD")
 BASIC_AUTH: tuple = (API_BASIC_AUTH_USERNAME, API_BASIC_AUTH_PASSWORD)
 
-GLOBAL_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "global/config")
+GLOBAL_CONFIGURATION: str = os.path.join(ONE_GREEN_API_URL, "global/config")
 
-WATER_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "water/config")
-WATER_FORCE_CONTROLLER: str = os.path.join(ONE_GREEN_CORE_API, "water/controller/force")
+WATER_CONFIGURATION: str = os.path.join(ONE_GREEN_API_URL, "water/config")
+WATER_FORCE_CONTROLLER: str = os.path.join(ONE_GREEN_API_URL, "water/controller/force")
 
-SPRINKLER_REGISTRY: str = os.path.join(ONE_GREEN_CORE_API, "sprinkler/registry")
-SPRINKLER_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "sprinkler/config")
-SPRINKLER_FORCE_CONTROLLER: str = os.path.join(ONE_GREEN_CORE_API, "sprinkler/controller/force")
+SPRINKLER_REGISTRY: str = os.path.join(ONE_GREEN_API_URL, "sprinkler/registry")
+SPRINKLER_CONFIGURATION: str = os.path.join(ONE_GREEN_API_URL, "sprinkler/config")
+SPRINKLER_FORCE_CONTROLLER: str = os.path.join(
+    ONE_GREEN_API_URL, "sprinkler/controller/force"
+)
 
-LIGHT_REGISTRY: str = os.path.join(ONE_GREEN_CORE_API, "light/registry")
-LIGHT_CONFIGURATION: str = os.path.join(ONE_GREEN_CORE_API, "light/config")
+LIGHT_REGISTRY: str = os.path.join(ONE_GREEN_API_URL, "light/registry")
+LIGHT_CONFIGURATION: str = os.path.join(ONE_GREEN_API_URL, "light/config")
